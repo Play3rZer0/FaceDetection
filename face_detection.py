13f058787a544059081e70cab70d7402d4e4c2ea
Play3rZer0 <37818562+Play3rZer0@users.noreply.github.com>
2019-02-01 02:57:11 +0800

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,32 @@
+# load the library using the import keyword
+import cv2
+import sys
+
+# Gets the name of the image file (filename) from sys.argv
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# This creates the cascade classifcation from file 
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# The image is read and converted to grayscale
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# The face or faces in an image are detected
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(1,1),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+
+print("Detected {0} faces!".format(len(faces)))
+
+# This draws a green rectangle around the faces detected
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces Detected", image)
+cv2.waitKey(0)
